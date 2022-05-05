f213d4ab36e8010217ef73acd44581e85372ef3a
Joshua Bultman <joshuab1239@gmail.com>
2022-05-05 13:14:15 -0500

    added basic function to josh_testing class

--- Testing/src/testing/Josh_testing.java
+++ Testing/src/testing/Josh_testing.java
@@ -1,7 +1,14 @@
 package testing;
 
+import java.util.Scanner;
+
 public class Josh_testing {
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main1() {
 		System.out.println("Welcome josh");
+		
+		//calls back to main Testing.java class
+//		Testing.main(null);
 	}
 }
